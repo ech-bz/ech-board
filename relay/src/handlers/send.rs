c164966d4708d60f3f48ec7f953d02e874177a28
foobar <admin@ech.bz>
2026-07-21 02:45:19 +0300

    relay/

--- relay/src/handlers/send.rs
+++ relay/src/handlers/send.rs
@@ -32,8 +32,7 @@ trait IntentPayload: Send + Sync {
 
 #[derive(Deserialize)]
 struct NewThreadPayload {
-    #[allow(dead_code)]
-    subject: String,
+    topic_hash: Option<Address>,
     text_hash: Option<Address>,
     media_hashes: Vec<Address>,
 }
